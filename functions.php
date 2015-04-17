bd0f751c3473c4f0a4d655b72c5764fba0093927
Jake Bahnsen <mrjake79@gmail.com>
2015-04-16 20:13:47 -0600

    Changes to login functionality

--- functions.php
+++ functions.php
@@ -8,3 +8,21 @@ add_action('wp_enqueue_scripts', function() {
 add_action('after_setup_theme', function() {
     remove_theme_support('custom-header');
 }, 12);
+
+// Custom login redirect
+add_filter('login_redirect', function($redirect_to, $request, $user) {
+    if(is_array($user->roles) && in_array('administrator', $user->roles)) {
+        return admin_url();
+    } elseif($user->id) {
+        return admin_url() . 'index.php';
+    } else {
+        return site_url();
+    }
+}, 10, 3);
+
+// Customize the login page
+if(is_plugin_active('wordpress-social-login')) {
+    add_action('login_enqueue_scripts', function() {
+        wp_enqueue_style('custom-login', get_stylesheet_directory_uri() . '/style-login.css');
+    });
+}
